09a52963555f6457ddc25f2b012dab6ddff4d601
owenmarcuslether <omarc484@evit.edu>
2024-08-30 13:10:08 -0700

    gorp giepy

--- # Toolbox the Ancient Griefman.py
+++ # Toolbox the Ancient Griefman.py
@@ -1,8 +1,6 @@
-# Toolbox the Ancient Griefman
-# or any of it's variants, really. This is a game from SS13 that is infamously easy to code.  In the interest of gatekeeping, don't look into my obscure and niche interests
-
 import pygame
 import random
+import sys
 
 atk = False
 heal = False
@@ -10,7 +8,7 @@ charge = False
 
 verbnamecandidates = ["Pulverize", "Destroy", "Defile", "Gib", "Crush", "Annoy", "Defeat", "Annihilate", "Obliterate", "Slaughter"]
 adjnamecandidates = ["Ancient", "Monstrous", "Vile", "Pompous", "Ugly", "Revolting", "Terrible", "Awful","Deceptive", "Insane"]
-nounnamecandidates = ["Griefman", "Grifter", "Spacer", "Moth", "Lich King", "Greyshirt", "Monster", "Vampire", "Opponent", "Melee Attacker"]
+nounnamecandidates = ["Villain", "Vagabond", "Hobo", "Cyborg", "Lich King", "Janitor", "Monster", "Vampire", "Opponent", "Melee Attacker"]
 verbname = verbnamecandidates[random.randint(0,9)]
 adjname = adjnamecandidates[random.randint(0,9)]
 nounname = nounnamecandidates[random.randint(0,9)]
@@ -20,22 +18,33 @@ title = (verbname + " The " + adjname + " " + nounname)
 white = (255, 255, 255)
 black = (0, 0, 0)
 blue = (0, 0, 128)
-X = 800
-Y = 800
-display_surface = pygame.display.set_mode((X, Y))
+width = 800
+height = 800
+screen = [width, height]
+mouse = (0,0,0)
+smallfont = pygame.font.SysFont('Corbel',35) 
+display_surface = pygame.display.set_mode((width, height))
 pygame.display.set_caption(title)
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render(title, True, white, blue)
 textRect = text.get_rect()
-textRect.center = (400, 100)
+textRect.center = (width // 2, height // 4)  # Moved title text higher
 
-while True:
-    display_surface.fill(blue)
+while True: 
+    for ev in pygame.event.get(): 
+        if ev.type == pygame.QUIT: 
+            pygame.quit() 
+            sys.exit()
+        if ev.type == pygame.MOUSEBUTTONDOWN: 
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.quit() 
+                sys.exit()
+    
+    display_surface.fill((60,25,123))
+    mouse = pygame.mouse.get_pos() 
+    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+        pygame.draw.rect(display_surface,white,[width/2,height/2,140,40]) 
+    else: 
+        pygame.draw.rect(display_surface,black,[width/2,height/2,140,40]) 
     display_surface.blit(text, textRect)
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            atk = True
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        pygame.display.update()
+    pygame.display.update()
--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,19 +1,25 @@
-# Tic Tac Toe 
+# Abandoned and likely that I won't come back to it. Nonfunctional.
+
+
+
+
+import random
+
 turn = False
-aa = "   "
-ab = "   "
-ac = "   "
-ba = "   "
-bb = "   "
-bc = "   "
-ca = "   "
-cb = "   "
-cc = "   "
-spots = [aa,ab,ac,ba,bb,bc,ca,cb,cc]
+spots = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 corners = 0
 botmove = 0
+randombsgo = 0
+dp1 = False
+dp2 = False
+dp3 = False
+dp4 = False
 
+def checkend():
+    pass
+    
 def playerturn():
+    print(spots)
     playermove = int(input("Select a square to mark with an X. Use single digit, non-zero numbers only."))
     playermove = playermove - 1
     if playermove < 9 and playermove > -1:
@@ -26,32 +32,126 @@ def playerturn():
         playerturn()
 
 def botturn():
-    if ba == " X " and bc == " X " or ab == " X " and cb == " X ": #Emergency block off if they're gonna win by avoiding the corners
-        if bb != " O " or bb != " X ":
-            bb = " O "
+    global dp1, dp2, dp3, dp4
+    global spots
+    global corners
+    
+    if corners == 2: # Identify the endgame. It got you right where it wants you
+        if spots[0] == " O " and spots[6] == " O ":
+            if spots[8] == "   ":
+                spots[8] = " O "
+                dp1 = True
+                playerturn()
+                pass
+            elif spots[2] == "   ":
+                spots[2] = " O "
+                dp2 = True
+                playerturn()
+            else:
+                pass
+        if spots[8] == " O " and spots[2] == " O ":
+            if spots[0] == "   ":
+                spots[0] = " O "
+                dp3 = True
+                playerturn()
+            elif spots[6] == "   ":
+                spots[6] = " O "
+                dp4 = True
+                playerturn()
+            else: 
+                pass
         else:
             pass
 
+    if dp1 == True: 
+        if spots[7] == "   ":
+            spots[7] = " O "
+            checkend()
+            playerturn()
+    elif spots[3] == "   ":
+            spots[3] = " O "
+            checkend()
+            playerturn()
+    if dp2 == True:
+        if spots[1] == "   ":
+            spots[1] = " O "
+            checkend()
+            playerturn()
+    elif spots[3] == "   ":
+            spots[3] = " O "
+            checkend()
+            playerturn()
+    if dp3 == True:
+        if spots[1] == "   ":
+            spots[1] = " O "
+            checkend()
+            playerturn()
+    elif spots[5] == "   ":
+            spots[5] = " O "
+            checkend()
+            playerturn()
+    if dp4 == True:
+        if spots[7] == "   ":
+            spots[7] = " O "
+            checkend()
+            playerturn()
+    elif spots[5] == "   ":
+            spots[5] = " O "
+            checkend()
+            playerturn()
+      
+    if (spots[3] == " X " and spots[5] == " X ") or (spots[1] == " X " and spots[7] == " X "): #Emergency block if they're gonna win by avoiding the corners
+        if spots[4] == "   ":
+            spots[4] = " O "
+            print("DEBUG: MIDBLOCKED")
+            playerturn()
+        else:
+            pass
 
-    if aa == "   ":
-        aa = " O "
+    if spots[1] == " X " and spots[4] == " X ": #Emergency block if they're gonna win by rushing the middle (vertical)
+        if spots[4] == "   ":
+            spots[4] = " O "
+            playerturn()
+        else:
+            pass
+    if spots[3] == " X " and spots[5] == " X ": #Emergency block if they're gonna win by rushing the middle (horizontal)
+        if spots[4] == "   ":
+            spots[4] = " O "
+            playerturn()
+        else:
+            pass
+
+    if spots[0] == "   ": 
+        spots[0] = " O "
         playerturn()
-    elif ac == "   ":
-        ac = " O "
-        playerturn()
-    elif ca == "   ":
-        ca = " O "
-        playerturn()
-    elif cc == "   " and corners <3:
-        cc = " O "
-        playerturn()
-       
-    if corners == 3:
         pass
-    print(spots)
+    elif spots[2] == "   ":
+        spots[2] = " O "
+        playerturn()
+        pass
+    elif spots[8] == "   ":
+        spots[8] = " O "
+        playerturn()
+        pass
+    elif spots[6] == "   " and corners < 2:
+        spots[6] = " O "
+        playerturn()
+        pass
+    elif spots[2] == "   " and corners < 2:
+        spots[2] = " O "
+        playerturn()
+        pass
+    else:
+        pass
+       
+
+    randombsgo = random.randint(0, 8) 
+    if spots[randombsgo] == "   ":
+        spots[randombsgo] = " O "
+        print("DEBUG: Random move made")
     playerturn()
 
-if input("Will you go first or second? Return a 1 or a 2.") == ("1"):
-    playerturn()
+if input("Will you go first or second? Return a 1 or a 2.") == ("1"): 
+    spots[0] = " O "
 else:
-    botturn()
+    playerturn()
